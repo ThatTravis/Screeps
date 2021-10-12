dad5a0305e9d0adb8c4af9da36dcf5ff909a40a8
ThatTravis <michaeljuengling@hotmail.de>
2021-10-12 15:17:42 +0200

    Update

---
+++ role.tester.js
@@ -0,0 +1,40 @@
+//TEST CREEP for builder
+
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('harvest');
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('build');
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+	    else {
+            var targets = creep.room.spawns[0]
+            creep.say(targets.store.getFreeCapacity(RESOURCE_ENERGY));
+            if(creep.withdraw(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets);
+            }
+        }
+	}
+};
+
+module.exports = roleBuilder;
+
+
+
+
+
+
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,19 +1,30 @@
+
+
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('harvest');
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('upgrade');
+	    }
+
+	    if(creep.memory.building) {
+	        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
-        }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-	}
+	    }
+    }
 };
 
 module.exports = roleUpgrader;
